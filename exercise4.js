80549af3d3fce246d5ebf55b62af0345b13a7ef5
mincTeja <himateja135@gmail.com>
2021-02-14 08:58:57 +0530

    trying merge conflict

--- exercise4.js
+++ exercise4.js
@@ -3,6 +3,6 @@ window.addEventListener('load',()=>{
     const inputField = document.querySelector(".text-input");
     inputField.addEventListener('input',()=>{
         // console.log("input value : "+inputField.value+" "+inputField.value.length);
-        charLen.innerHTML = inputField.value.length;
+        charLen.innerHTML = inputField.value.length();
     });
 });
